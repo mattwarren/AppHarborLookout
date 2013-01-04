95818aade19fdc4d3deba138f0fc707870faf6aa
marcel-valdez <marcel.valdez@live.com>
2013-01-04 02:21:02 -0600

    Added a test project (incomplete tests) and refactored main app (needs some redesign)

---
+++ AppHarbor.Lookout/BuildInfo.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using AppHarbor;
+using AppHarbor.Model;
+
+namespace AppHarborLookout {
+  public class BuildInfo {
+
+    private AppHarborClient mClient;
+    private IEnumerable<Application> mApplications;
+    private string mApplicationName;
+    private Build mLatestBuild = null;
+    private string mLatestAppId = null;
+    private IEnumerable<Error> mErrors;
+    private bool mCacheObtained = false;
+
+    public BuildInfo(IEnumerable<Application> applications, AppHarborClient client) {
+      Contract.Requires(applications != null);
+      Contract.Requires(client != null);
+
+      this.mApplications = applications;
+      this.mClient = client;      
+    }    
+
+    /// <summary>
+    /// Gets the name of the application.
+    /// </summary>
+    /// <value>The name of the application.</value>
+    public string ApplicationName {
+      get {        
+        if(!mCacheObtained) {          
+          UpdateCache();
+        }
+
+        return mApplicationName;
+      }
+    }
+
+    /// <summary>
+    /// Gets the latest build.
+    /// </summary>
+    /// <value>The latest build.</value>
+    public Build LatestBuild {
+      get {
+        if(!mCacheObtained) {
+          UpdateCache();
+        }
+
+        return mLatestBuild;
+      }
+    }
+
+    /// <summary>
+    /// Gets the errors.
+    /// </summary>
+    /// <value>The errors.</value>
+    public IEnumerable<Error> Errors {
+      get {
+        if(!mCacheObtained) {
+          UpdateCache();
+        }
+
+        return this.mErrors;
+      }
+    }
+
+    /// <summary>
+    /// Updates the cache.
+    /// </summary>
+    private void UpdateCache() {
+      var latestApp = mApplications.Select(app => new {
+        AppName = app.Name,
+        AppId = app.Slug,
+        LatestBuild = mClient.GetBuilds(app.Slug)
+                            .OrderByDescending(build => build.Created)
+                            .FirstOrDefault()
+      })
+      .OrderByDescending(app => app.LatestBuild.Created)
+      .FirstOrDefault();
+
+      if(latestApp != null) {
+        this.mApplicationName = latestApp.AppName;
+        this.mLatestAppId = latestApp.AppId;
+        this.mLatestBuild = latestApp.LatestBuild;
+        this.mErrors = mClient.GetErrors(this.mLatestAppId);
+      }
+    }
+  }
+}
---
+++ AppHarbor.Lookout/BuildStatusInterpreter.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using AppHarborLookout.Properties;
+
+namespace AppHarborLookout {
+  public class BuildStatusInterpreter {
+    /// <summary>
+    /// Gets the build status code for a status string.
+    /// </summary>
+    /// <param name="status">The status string.</param>
+    /// <returns>A BuildStatusCode.</returns>
+    public BuildStatus GetBuildStatus(string status) {
+      switch(status.ToLowerInvariant()) {
+        case "failed":
+          return BuildStatus.Failed;
+        case "succeeded":
+          return BuildStatus.Succeeded;
+        case "queued":
+          return BuildStatus.Queued;
+        case "building":
+          return BuildStatus.Building;
+      }
+
+      return BuildStatus.Unknown;
+    }
+
+    /// <summary>
+    /// Gets the build status message.
+    /// </summary>
+    /// <param name="current">The current.</param>
+    /// <returns>The System.String message.</returns>
+    public string GetBuildStatusMsg(BuildStatus current) {
+      switch(current) {
+        case BuildStatus.Succeeded:
+          return "Good job. The Build succeeded.";
+        case BuildStatus.Failed:
+          return "The build is broken :(";
+        default:
+          return null;
+      }
+    }
+
+    /// <summary>
+    /// Gets the colour for build status.
+    /// </summary>
+    /// <param name="currentBuildStatus">The current build status.</param>
+    /// <returns>Color.</returns>
+    public Color GetBuildStatusColor(BuildStatus currentBuildStatus) {
+      switch(currentBuildStatus) {
+        case BuildStatus.Failed:
+          return Color.Red;
+        case BuildStatus.Building:
+        case BuildStatus.Queued:
+          return Color.Orange;
+        case BuildStatus.Succeeded:
+          return Color.Green;
+        default:
+          return Color.Gray;
+      }
+    }
+
+    /// <summary>
+    /// Gets the build status notify icon.
+    /// </summary>
+    /// <param name="status">The status.</param>
+    /// <returns>Icon.</returns>
+    public Icon GetBuildStatusNotifyIcon(BuildStatus status) {
+      switch(status) {
+        case BuildStatus.Failed:
+          return Resources.RedButton;
+        case BuildStatus.Succeeded:
+          return Resources.GreenButton;
+        case BuildStatus.Queued:
+        case BuildStatus.Building:
+          return Resources.OrangeButton;
+        default:
+          return Resources.GrayButton;
+      }
+    }
+  }
+
+  public enum BuildStatus {
+    Building,
+    Failed,
+    Queued,
+    Succeeded,
+    Unknown
+  }
+}
